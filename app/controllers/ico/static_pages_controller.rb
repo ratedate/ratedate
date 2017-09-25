e31ca85e2439c48f2a386d631965cd94685ca623
Dmitry Nadsadin <leloushlampergh@gmail.com>
2017-09-25 23:28:41 +0300

    small changes

--- app/controllers/ico/static_pages_controller.rb
+++ app/controllers/ico/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class Ico::StaticPagesController < Ico::IcoController
   def ico_landing
   end
+  def in_develop
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,13 @@ Rails.application.routes.draw do
     scope module: 'ico' do
       scope "/:locale", locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "ru"}  do
         get 'static_pages/ico_landing'
-        root 'static_pages#ico_landing'
+        get 'static_pages/in_develop'
+        root 'static_pages#in_develop'
       end
-      root 'static_pages#ico_landing'
+      root 'static_pages#in_develop'
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     end
-    root 'static_pages#ico_landing', module: 'ico'
+    root 'static_pages#in_develop', module: 'ico'
   end
 
   root 'static_pages#in_develop'
