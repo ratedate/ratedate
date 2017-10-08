c991dd6aa96c421e94421f0866b99809a2618633
Dmitry Nadsadin <leloushlampergh@gmail.com>
2017-10-08 20:36:14 +0300

    js changes, include js only for controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
     tags = '<meta name="viewport" content="width=device-width, initial-scale=1.0">'
     tags.html_safe
   end
+  def google_analytics
+    ga = "<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src='https://www.googletagmanager.com/gtag/js?id=UA-107749703-1'></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-107749703-1');
+</script>"
+    ga.html_safe
+  end
 end
