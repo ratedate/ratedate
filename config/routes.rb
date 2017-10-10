eef18b78377d826366827c49e42c05a95c5eb1de
Дмитрий «Чеширский Кот» Надсадин <leloushlampergh@gmail.com>
2017-10-10 20:31:16 +0000

    Revert "devise + omniauth"
    This reverts commit 824905950a4222f797cec141ce4375435af42865

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
-  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   scope "/:locale", locale: /#{I18n.available_locales.join("|")}/, defaults: {locale: "en"}  do
     get 'static_pages/home'
     get 'static_pages/in_develop'
