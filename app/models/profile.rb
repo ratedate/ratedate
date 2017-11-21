7b0862ea1e19e41a47c2e7b985e88da338517098
Nadsadin <leloushlampergh@gmail.com>
2017-11-21 18:03:48 +0300

    taggings for profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,5 +3,6 @@ class Profile < ApplicationRecord
   # Used for user avatar image edit
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
   mount_uploader :avatar, AvatarUploader
-
+  # acts_as_taggable
+  acts_as_ordered_taggable_on :hobbies, :musics, :films, :books
 end
