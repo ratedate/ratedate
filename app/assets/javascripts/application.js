642ee3f8959319781e50b4e8b33927a5e75c15f9
Nadsadin <leloushlampergh@gmail.com>
2018-03-03 09:31:08 +0300

    no async

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -189,7 +189,7 @@ var ready = function() {
     });
 };
 
-$(document).on('turbolinks:load ready', ready);
+$(document).on('turbolinks:load', ready);
 
 function handleFullpageOnLeave(index, nextIndex, direction) {
     if (nextIndex > 2) {
