ee886d2da5ea99aee9161dcb68219aa6c054f7b4
Nadsadin <leloushlampergh@gmail.com>
2018-06-14 14:17:50 +0300

    fix if user havent balance

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,6 +5,9 @@ class Payment < ApplicationRecord
   attr_accessor :response_status, :response_signature_string
 
   def complete
+    if self.user.balance.nil?
+      self.user.create_balance()
+    end
     self.user.balance.add_amount(rdt_amount)
     self.transactions.create(balance_id: user.balance.id, amount: rdt_amount, transaction_type: 'payment')
   end
